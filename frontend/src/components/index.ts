ce126c691272ca4b21563e4066860acb32593205
shreya-nair21 <shreyanair2104@gmail.com>
2026-06-24 14:16:42 +0530

    export navbar

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,3 +2,4 @@ export { Hero } from './Hero';
 export { Navbar } from './Navbar';
 export { Button } from './Button';
 export { Section, SectionHeading } from './Section';
+export { default as TripSubNavbar } from './TripSubNavbar';
